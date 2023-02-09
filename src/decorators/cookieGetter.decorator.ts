daf1c3e7d875b740917566768d7648fa2e2cb333
Jamalov <Jamalovn07@gmail.com>
2023-02-09 17:57:56 +0500

    add

--- src/decorators/cookieGetter.decorator.ts
+++ src/decorators/cookieGetter.decorator.ts
@@ -7,10 +7,12 @@ import {
 export const cookieGetter = createParamDecorator(
   async (data: 'refresh_token', context: ExecutionContext): Promise<string> => {
     const request = context.switchToHttp().getRequest();
-    console.log(request);
-    console.log(request.cookies);
+    // console.log(request);
+    console.log(request.headers);
+    // console.log(request.cookies);
 
-    const refreshToken = request.cookies[data];
+    const refreshToken = request.headers.authorization.split(" ")[1];
+    console.log(refreshToken)
     if (!refreshToken) {
       throw new UnauthorizedException('admin unauthorized decorator');
     }
